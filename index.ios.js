6c002bf62b7a728c14701dd770e6d324e4a60361
Francisco Sales <franciscofsales1@gmail.com>
2016-11-10 23:12:47 +0000

    button, drag, color

--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,116 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Animated,
+  Easing,
+  TouchableWithoutFeedback,
+  PanResponder
 } from 'react-native';
 
 export default class reactNativeAnimationExample extends Component {
+  constructor(props){
+    super(props);
+    this.handlePressIn = this.handlePressIn.bind(this);
+    this.handlePressOut = this.handlePressOut.bind(this);
+  }
+  componentWillMount() {
+    // button
+    this.animatedValue = new Animated.Value(1);
+
+    // drag box
+    this.panAnimatedValue = new Animated.ValueXY();
+    this._panValue = {x:0, y:0};
+    this.panAnimatedValue.addListener((value) => {this._panValue = value});
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderGrant: (e, gestureState) => {
+        this.panAnimatedValue.setOffset({
+          x: this._panValue.x,
+          y: this._panValue.y
+        });
+        this.panAnimatedValue.setValue({x:0, y:0});
+      },
+      onPanResponderMove: Animated.event([
+        null,
+        {dx: this.panAnimatedValue.x, dy: this.panAnimatedValue.y}
+      ]),
+      onPanResponderRelease: (e, gestureState) => {
+        this.panAnimatedValue.flattenOffset();
+        Animated.decay(this.panAnimatedValue, {
+          deceleration: 0.993,
+          velocity:{x:gestureState.vx, y:gestureState.vy}
+        }).start();
+      }
+    });
+
+    // COlor animation
+    this.colorAnimatedValue = new Animated.Value();
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  // Button
+  handlePressIn() {
+    Animated.spring( this.animatedValue, {
+      toValue: 0.5
+    }).start();
+  }
+
+  handlePressOut() {
+    Animated.spring( this.animatedValue, {
+      toValue: 1,
+      friction: 3,
+      tension: 40
+    }).start();
+  }
+
+  //----
+
   render() {
+    // button
+    const animatedStyle = {
+      transform: [
+        {scale: this.animatedValue}
+      ]
+    };
+    // Drag button
+    const panAnimatedStyle = {
+      transform: this.panAnimatedValue.getTranslateTransform()
+    };
+    //color animation
+    const interpolateColor = this.colorAnimatedValue.interpolate({
+      inputRange: [0, 150],
+      outputRange: ['rgb(0, 0, 0)', 'rgb(52, 250, 170)']
+    });
+    const colorAnimatedStyle = {
+      backgroundColor: interpolateColor
+    };
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TouchableWithoutFeedback
+          onPressIn={this.handlePressIn}
+          onPressOut={this.handlePressOut}
+        >
+          <Animated.View style={[styles.box, animatedStyle]}>
+            <Text style={styles.textStyle}>Press me</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
+
+        <Animated.View style={[styles.box, panAnimatedStyle]} {...this.panResponder.panHandlers}>
+          <Text style={styles.textStyle}>Drag me</Text>
+        </Animated.View>
+
+        <Animated.View style={[styles.box, colorAnimatedStyle]} {...this.panResponder.panHandlers}>
+          <Text style={styles.textStyle}>Check my color</Text>
+        </Animated.View>
       </View>
     );
   }
@@ -34,20 +119,21 @@ export default class reactNativeAnimationExample extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding:30,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  box: {
+    width: 100,
+    height: 50,
+    backgroundColor: '#333',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  textStyle: {
+    color: 'white'
+  }
 });
 
 AppRegistry.registerComponent('reactNativeAnimationExample', () => reactNativeAnimationExample);
